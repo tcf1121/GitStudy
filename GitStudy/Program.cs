94a4cd0c375a87e5306f180c18219f9de96b8199
FeAnd <tcf1268@gmail.com>
2025-03-20 12:34:30 +0900

    Update Program.cs
    Program.cs 코드 수정

--- GitStudy/Program.cs
+++ GitStudy/Program.cs
@@ -10,10 +10,12 @@ namespace GitStudy
     {
         static void Main(string[] args)
         {
+            int age;
             Console.Write("이름을 입력해주세요 : ");
             string input = Console.ReadLine();
-
-            Console.WriteLine("입력하신 이름은 {0} 입니다.", input);
+            Console.WriteLine("나이를 입력해주세요 : ");
+            int.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine("입력하신 이름은 {0} 나이는 {1} 입니다.", input, age);
         }
     }
 }
